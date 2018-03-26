54a2356ef09fbc346d2f4943022ffe18367d019e
toringo <zg-gwt@zg18.com>
2018-03-26 17:51:16 +0800

    fix： 去掉8013端口

--- api/wx/controllers/Wx.js
+++ api/wx/controllers/Wx.js
@@ -182,7 +182,7 @@ module.exports = {
                     const wxuserinfo = yield request(`https://www.13cai.com.cn/wxuserinfo?${qs.stringify(option)}`);
                     console.log('更新user表结果', qs.stringify(wxuserinfo))
                     this.sttaus = 302;
-                    this.redirect(`https://www.13cai.com.cn:8013?${qs.stringify(wxuserinfo)}`);
+                    this.redirect(`https://www.13cai.com.cn?${qs.stringify(wxuserinfo)}`);
                 } else {
                     this.body = '未知错误，请退出重试';
                 }
