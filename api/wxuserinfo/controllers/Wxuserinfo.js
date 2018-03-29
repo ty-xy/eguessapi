417258e016513311828cf708df1597bfa72c7f1a
toringo <zg-gwt@zg18.com>
2018-03-29 19:15:26 +0800

    fix: 存在用户表返回信息出错

--- api/wxuserinfo/controllers/Wxuserinfo.js
+++ api/wxuserinfo/controllers/Wxuserinfo.js
@@ -56,9 +56,11 @@ module.exports = {
                 }
                 this.body = userUpdate[0];
             }else{
-                const users =yield Wxuserinfo.update({id:entry.id},{query})
+                const users = yield Wxuserinfo.update({id:entry.id},{query})
                 console.log("Wxuserinfo存在，则更新user",users, users.wxUser)
-                this.body = users[0];
+                const _user = yield user.findOne({id: users.wxUser})
+                console.log('_user', _user)
+                this.body = _user;
             }
         }else{
             this.body = {};
